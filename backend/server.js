645aa417a60dd92ff1b507350792e65ba9a1e5d0
Srijan Dawn <srijandawn13@gmail.com>
2025-02-15 14:36:33 +0530

    4th

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
@@ -11,8 +12,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-require('dotenv').config();
-mongoose.connect(`${process.env.MONGODB_URI}`);
+// console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('✅ MongoDB Connected'))
+.catch(err => console.error('❌ MongoDB Connection Error:', err));
 
 // Define a schema and model
 const UserSchema = new mongoose.Schema({
